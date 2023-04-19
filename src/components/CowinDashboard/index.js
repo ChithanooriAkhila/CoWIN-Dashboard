22fecae1224adc2f2857d9ac8184cb5670349952
ChithanooriAkhila <cakhila982@gmail.com>
2023-04-19 17:01:54 +0000

    CoWIN Dashboard

---
+++ src/components/CowinDashboard/index.js
@@ -0,0 +1,126 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import VaccinationCoverage from '../VaccinationCoverage'
+import VaccinationByAge from '../VaccinationByAge'
+import VaccinationByGender from '../VaccinationByGender'
+
+const fetchingConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  loading: 'LOADING',
+}
+
+class CowinDashboard extends Component {
+  state = {
+    isLoading: fetchingConstants.initial,
+    last7DaysVaccination: [],
+    byAgeDetails: [],
+    byGenderDetails: [],
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = async () => {
+    this.setState({isLoading: fetchingConstants.loading})
+
+    const fetchUrl = 'https://apis.ccbp.in/covid-vaccination-data'
+    const response = await fetch(fetchUrl)
+
+    if (response.ok) {
+      const data = await response.json()
+      const last7DaysVaccinationDetails = data.last_7_days_vaccination.map(
+        obj => ({
+          vaccineDate: obj.vaccine_date,
+          dose1: obj.dose_1,
+          dose2: obj.dose_2,
+        }),
+      )
+
+      const ageDetails = data.vaccination_by_age.map(range => ({
+        age: range.age,
+        count: range.count,
+      }))
+      const genderDetails = data.vaccination_by_gender.map(genderType => ({
+        gender: genderType.gender,
+        count: genderType.count,
+      }))
+
+      this.setState({
+        isLoading: fetchingConstants.success,
+        last7DaysVaccination: last7DaysVaccinationDetails,
+        byAgeDetails: ageDetails,
+        byGenderDetails: genderDetails,
+      })
+    } else {
+      this.setState({
+        isLoading: fetchingConstants.failure,
+      })
+    }
+  }
+
+  renderCowinDashboardSuccessView = () => {
+    const {last7DaysVaccination, byAgeDetails, byGenderDetails} = this.state
+    return (
+      <div>
+        <VaccinationCoverage
+          vaccinationCoverageDetails={last7DaysVaccination}
+        />
+        <VaccinationByGender vaccinationByGenderDetails={byGenderDetails} />
+        <VaccinationByAge vaccinationByAgeDetails={byAgeDetails} />
+      </div>
+    )
+  }
+
+  renderCowinDashboardFailureView = () => (
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png"
+        alt="failure view"
+      />
+      <h1>Something went wrong</h1>
+    </div>
+  )
+
+  renderLoader = () => (
+    <div data-testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height={80} width={80} />
+    </div>
+  )
+
+  renderCowinDashboard = () => {
+    const {isLoading} = this.state
+
+    switch (isLoading) {
+      case fetchingConstants.success:
+        return this.renderCowinDashboardSuccessView()
+      case fetchingConstants.failure:
+        return this.renderCowinDashboardFailureView()
+      case fetchingConstants.loading:
+        return this.renderLoader()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+            alt="website logo"
+          />
+          <h1>Co-win</h1>
+        </div>
+        <h1>CoWIN Vaccination in India</h1>
+        <div>{this.renderCowinDashboard()}</div>
+      </>
+    )
+  }
+}
+
+export default CowinDashboard
---
+++ src/components/VaccinationByAge/index.js
@@ -0,0 +1,38 @@
+// Write your code here
+
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+
+const VaccinationByAge = props => {
+  const {vaccinationByAgeDetails} = props
+
+  return (
+    <>
+      <h1>Vaccination by Age</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={vaccinationByAgeDetails}
+            startAngle={0}
+            endAngle={360}
+            innerRadius="40%"
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="18-44" fill="#fecba6" />
+            <Cell name="44-60" fill="#b3d23f" />
+            <Cell name="above 60" fill="#a44c9e" />
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="horizontal"
+            verticalAlign="bottom"
+            align="center"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </>
+  )
+}
+export default VaccinationByAge
